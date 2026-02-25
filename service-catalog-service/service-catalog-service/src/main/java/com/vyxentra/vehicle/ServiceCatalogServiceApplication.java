770d75ab178fb5e99d15f434daa84ad0e96e3d16
unknown <dasariravid145@gmail.com>
2026-02-25 13:44:32 +0530

    Vyxentra is an enterprise-grade, microservices-based On-Demand Vehicle Service

---
+++ service-catalog-service/service-catalog-service/src/main/java/com/vyxentra/vehicle/ServiceCatalogServiceApplication.java
@@ -0,0 +1,13 @@
+package com.vyxentra.vehicle;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServiceCatalogServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ServiceCatalogServiceApplication.class, args);
+	}
+
+}
