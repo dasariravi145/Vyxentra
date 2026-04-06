c16cf8354905c0a2c0fc2eb332eb6553344371fc
unknown <dasariravid145@gmail.com>
2026-04-06 21:41:45 +0530

    payment service module changes done

---
+++ employee-service/employee-service/src/main/java/com/vyxentra/vehicle/exception/EmployeeException.java
@@ -0,0 +1,13 @@
+package com.vyxentra.vehicle.exception;
+
+
+
+public class EmployeeException extends RuntimeException {
+    public EmployeeException(String message) {
+        super(message);
+    }
+
+    public EmployeeException(String message, Throwable cause) {
+        super(message, cause);
+    }
+}
---
+++ employee-service/employee-service/src/main/java/com/vyxentra/vehicle/exception/GlobalExceptionHandler.java
@@ -0,0 +1,36 @@
+package com.vyxentra.vehicle.exception;
+
+
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import java.time.LocalDateTime;
+import java.util.HashMap;
+import java.util.Map;
+
+@RestControllerAdvice
+public class GlobalExceptionHandler {
+
+    @ExceptionHandler(EmployeeException.class)
+    public ResponseEntity<Map<String, Object>> handleEmployeeException(EmployeeException ex) {
+        Map<String, Object> response = new HashMap<>();
+        response.put("timestamp", LocalDateTime.now());
+        response.put("status", HttpStatus.BAD_REQUEST.value());
+        response.put("error", "Employee Error");
+        response.put("message", ex.getMessage());
+        return ResponseEntity.badRequest().body(response);
+    }
+
+    @ExceptionHandler(Exception.class)
+    public ResponseEntity<Map<String, Object>> handleGenericException(Exception ex) {
+        Map<String, Object> response = new HashMap<>();
+        response.put("timestamp", LocalDateTime.now());
+        response.put("status", HttpStatus.INTERNAL_SERVER_ERROR.value());
+        response.put("error", "Internal Server Error");
+        response.put("message", ex.getMessage());
+        return ResponseEntity.internalServerError().body(response);
+    }
+}
